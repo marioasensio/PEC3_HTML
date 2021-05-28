d1c5f0563e884c3950a679412a828f2b72668ed5
mario asensio <product@bechallenge.io>
2021-05-28 20:48:50 +0200

    miembros

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      'montserrat-regular': ['montserrat','sans-serif'],
+      'Limelight': ['Limelight','sans-serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    }
+  }
+}
+
